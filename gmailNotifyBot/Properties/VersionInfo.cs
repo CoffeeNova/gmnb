55d7d3212839af8c5c18f751896f15ef614e2fee
Igor Salzhenitsin <igor.sal@protonmail.com>
2017-05-10 22:37:36 +0300

    temp

--- gmailNotifyBot/Properties/VersionInfo.cs
+++ gmailNotifyBot/Properties/VersionInfo.cs
@@ -9,6 +9,6 @@
 //------------------------------------------------------------------------------
 
 [assembly: System.Reflection.AssemblyVersion("1.0")]
-[assembly: System.Reflection.AssemblyFileVersion("1.0.72.27")]
+[assembly: System.Reflection.AssemblyFileVersion("1.0.72.29")]
 
 
