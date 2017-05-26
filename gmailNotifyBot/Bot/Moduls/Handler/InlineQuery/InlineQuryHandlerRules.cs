d0664a8f9e3e1f5103a37ca45bacb8579584c186
Igor Salzhenitsin <igor.sal@protonmail.com>
2017-05-26 16:50:14 +0300

    temp

--- gmailNotifyBot/Bot/Moduls/Handler/InlineQuery/InlineQuryHandlerRules.cs
+++ gmailNotifyBot/Bot/Moduls/Handler/InlineQuery/InlineQuryHandlerRules.cs
@@ -7,7 +7,19 @@ using CoffeeJelly.gmailNotifyBot.Bot.Moduls.Handler.CallbackQuery;
 
 namespace CoffeeJelly.gmailNotifyBot.Bot.Moduls.Handler.InlineQuery
 {
-    internal class ShowMessagesRule : IInlineQueryHandlerRules
+    internal class ShowInboxMessagesRule : IInlineQueryHandlerRules
+    {
+        public HandleInlineQueryCommand Handle(InlineQueryHandler handler)
+        {
+            HandleInlineQueryCommand del = async sender => await handler.HandleShowMessagesInlineQueryCommand(sender);
+
+            if (data.Command.Equals(Commands.CONNECT_COMMAND, StringComparison.CurrentCultureIgnoreCase))
+                return del;
+            return null;
+        }
+    }
+
+    internal class ShowAllMessagesRule : IInlineQueryHandlerRules
     {
         public HandleInlineQueryCommand Handle(InlineQueryHandler handler)
         {
