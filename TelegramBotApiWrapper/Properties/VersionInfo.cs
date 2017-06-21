0605d04e21636ea897c7515cd7102947ece9e060
Igor Salzhenitsin <igor.sal@protonmail.com>
2017-06-21 16:58:18 +0300

    version update

--- TelegramBotApiWrapper/Properties/VersionInfo.cs
+++ TelegramBotApiWrapper/Properties/VersionInfo.cs
@@ -9,6 +9,6 @@
 //------------------------------------------------------------------------------
 
 [assembly: System.Reflection.AssemblyVersion("1.0")]
-[assembly: System.Reflection.AssemblyFileVersion("1.0.113.5")]
+[assembly: System.Reflection.AssemblyFileVersion("1.0.114.4")]
 
 
